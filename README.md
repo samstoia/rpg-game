dbb02ee77138d9ca8bc0981bb18f45e85c9e2ce6
LondresRi <lunsford.sk@gmail.com>
2019-02-06 15:31:56 -0800

    add layout in README for specs, add item and death descriptions

--- README.md
+++ README.md
@@ -20,8 +20,12 @@ Go'Leir is a text-based adventure role-playing game designed using HTML and Java
 
 | Behavior | Output | Input |
 |----------|:------:|:-----:|
-
-
+| | |
+| | |
+| | |
+| | |
+| | |
+| | |
 
 ## Setup and Use
 
--- descriptions/descriptions.js
+++ descriptions/descriptions.js
@@ -23,7 +23,7 @@ var monsterActions = [
 
 
 var itemDescriptions = [
-  
+
 ];
 
 var itemActions = [
@@ -32,3 +32,27 @@ var itemActions = [
   " has been left on the ground.",
   " lies nearby."
 ];
+
+// Descriptive Items
+var bareHandsDesc = "There are tales of great warriors who take down beasts with only their hands, with the strength to tear skull from spine and put an end to the plaguing horrors. Your hands don’t do that, you hit things and it hurts when you do."
+
+var quarterStaffDesc = "A stick that’s made for walking, and that’s just what it’ll do. It’s also excellent at striking down your foes with splintered rage. Though it’s a sturdy oak pole, the carvings on it seem worn and you know that it is very old.";
+
+var swordRustyDesc = "This sword once belonged to a knight of old who died in the most unfortunate of ways, and ever since his sword has passed from hand to hand, worn down by the ages. And terrible usage, it’s mainly been peasants using it to dig holes.";
+
+var swordBlessedDesc = "Awoken blade, a weapon of a hero from ages past. The blade is polished as a steel mirror, and it hums as it swings, cutting through the air-- and your enemies-- like wet paper.";
+
+var ironShieldDesc = "Out of all the breakthroughs in technology, the shield has become one of the most reliable, as it prevents things from hitting you. Well, more lessens things from hitting you than preventing."
+
+var healthPotionDesc = "A strange glowing red liquid that is made from organically raised and processed chicken blood infused with uranium.";
+
+var fireScrollDesc = "An ancient sheet torn from a tome of magic, capable of creating flames from nothing even if the user is magically inept. Or just inept. Not judging, just calling it like the scroll sees it.";
+
+// Descriptive Deaths
+var deathStarve = "You seem confused, lost, wandering back and froth with no goal in mind. Time passes and you forget that too, you forget to think, forget to feel. You forgot to eat. You starve yourself to death, wandering around doing nothing.";
+
+var deathCrone = "You lunge at the crone, thinking her a defenseless old woman. You thought wrong, and then you no longer have time to think anything at all. Your body catches fire as the ground beneath your feet crumbles, the very earth seizing your legs to crush your flesh and bones. You burn, trapped, reaching out for the old crone. Her leer is the last thing you see, before you see nothing at all, but the abyss of death.";
+
+var deathMonster = "Defeated, you lay a bloody mess on the ground. Your life plays out before your eyes, you think of home and the family you had, childhood friends and sweethearts. It’s been a good life, up to this point, you die with few regrets. Except that your skull is being turned into mash by the sadistic creature that claims victory over you.";
+
+var deathDragon = "You fought the good fight, your best fight, you gave it your all but lay in defeat. A rumbling snarl above your head, then nothing, a dread silence that causes you to look up. The dragon inhales sharply, before it bellows its fire onto you with a great roar. The pain is brief as your flesh crisps up and your innards cook, a delicacy for the dragon to enjoy, in one swift bite.";
